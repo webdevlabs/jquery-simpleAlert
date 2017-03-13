9b4ea60d85e20f5cce63c04a496c2a416d1b7f3f
Web Development Labs <webdevlabs@users.noreply.github.com>
2017-03-13 17:17:03 +0200

    Update and rename jquery.simplealert.js to jquery.simpleAlert.js

--- jquery.simpleAlert.js
+++ jquery.simpleAlert.js
@@ -1,11 +1,7 @@
 /* Simple Alert Handler
 Author: Simeon Lyubenov (lyubenov@gmail.com)
 Website: www.webdevlabs.com
-Usage:
-		<button data-alert="Click-click! Who's there?">click me!</button>
-Attach handler:
-		$('[data-alert]').AlertHandler();
-**/
+*/
 (function( $ ) {
 	$.fn.AlertHandler = function() {
 		this.click(function(){
